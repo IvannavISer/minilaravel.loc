ade2f1ce72ad478176ffa1e567da94a1b0f23ff9
Ivan <pyano.wolk@gmail.com>
2017-12-25 21:45:17 +0300

    дополнительная проверка на аутентификацию

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use Auth;
 class AdminController extends Controller
 {
     public function show(){
+        if(!Auth::check()){
+            return redirect('/login');
+        }
+        $user = Auth::user();
+        dump($user);
         return view('welcome');
     }
 }
